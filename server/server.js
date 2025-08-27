2a2a543e79cd97c8abaa3fbec3de34e764bd309a
Rishabh Rana <rishu2nd@gmail.com>
2025-08-27 14:47:26 +0530

    client file updated for vercel

--- server/server.js
+++ server/server.js
@@ -52,7 +52,9 @@ app.use("/api/messages", messageRouter);
 //connect to mongodb
 await connectDB();
 
-// const PORT = process.env.PORT || 5000;
-// server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
-
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
+}
+// export server for vercel
 export default app;
